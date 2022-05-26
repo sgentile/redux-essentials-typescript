b8edd2eb5874eb4644f54d8aa0b36f62fabe9411
Steve Gentile <steven.gentile@gmail.com>
2022-05-26 18:44:19 -0400

    updated save post

--- src/features/posts/EditPostForm.tsx
+++ src/features/posts/EditPostForm.tsx
@@ -1,56 +1,81 @@
-import React, { ChangeEventHandler, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { ChangeEventHandler, useEffect, useState } from 'react'
 import { RouteComponentProps, useHistory } from 'react-router-dom'
-import { postUpdated, selectPostById } from './postSlice'
+import { Spinner } from '../../components/Spinner'
+import { useGetPostQuery, useUpdatePostMutation } from '../api/apiSlice'
 
 type TParams = { postId: string }
 export const EditPostForm = ({ match }: RouteComponentProps<TParams>) => {
   const { postId } = match.params
 
-  const post = useSelector(state => selectPostById(state, postId))
+  // const post = useSelector((state) => selectPostById(state, postId))
+  const {
+    data: post,
+    isFetching,
+    isSuccess,
+    isError,
+    error,
+  } = useGetPostQuery(postId)
+  const [updatePost, { isLoading: isUpdating }] = useUpdatePostMutation()
 
   const [title, setTitle] = useState(post?.title ?? '')
   const [content, setContent] = useState(post?.content ?? '')
 
-  const dispatch = useDispatch()
+  useEffect(() => {
+    if (post) {
+      setTitle(post.title)
+      setContent(post.content)
+    }
+  }, [post])
+
   const history = useHistory()
 
-  const onTitleChanged: ChangeEventHandler<HTMLInputElement> =
-    (e) => setTitle(e.target.value)
-  const onContentChanged: ChangeEventHandler<HTMLTextAreaElement> =
-    (e) => setContent(e.target.value)
+  const onTitleChanged: ChangeEventHandler<HTMLInputElement> = (e) =>
+    setTitle(e.target.value)
+  const onContentChanged: ChangeEventHandler<HTMLTextAreaElement> = (e) =>
+    setContent(e.target.value)
 
-  const onSavePostClicked = () => {
+  const onSavePostClicked = async () => {
+    debugger
     if (title && content) {
-      dispatch(postUpdated({ id: postId, title, content }))
+      // dispatch(postUpdated({ id: postId, title, content }))
+      const updatedPost = { ...post, title, content }
+      debugger
+      await updatePost(updatedPost)
       history.push(`/posts/${postId}`)
     }
   }
-
-  return (
-    <section>
-      <h2>Edit Post</h2>
-      <form>
-        <label htmlFor="postTitle">Post Title:</label>
-        <input
-          type="text"
-          id="postTitle"
-          name="postTitle"
-          placeholder="What's on your mind?"
-          value={title}
-          onChange={onTitleChanged}
-        />
-        <label htmlFor="postContent">Content:</label>
-        <textarea
-          id="postContent"
-          name="postContent"
-          value={content}
-          onChange={onContentChanged}
-        />
-      </form>
-      <button type="button" onClick={onSavePostClicked}>
-        Save Post
-      </button>
-    </section>
-  )
+  let output
+  if (isError) {
+    output = <div>{error}</div>
+  } else if (isFetching || isUpdating) {
+    output = <Spinner text="Loading..." />
+  } else if (isSuccess) {
+    output = (
+      <>
+        <h2>Edit Post</h2>
+        <form>
+          <label htmlFor="postTitle">Post Title:</label>
+          <input
+            type="text"
+            id="postTitle"
+            name="postTitle"
+            placeholder="What's on your mind?"
+            value={title}
+            onChange={onTitleChanged}
+          />
+          <label htmlFor="postContent">Content:</label>
+          <textarea
+            id="postContent"
+            name="postContent"
+            value={content}
+            onChange={onContentChanged}
+          />
+        </form>
+        <button type="button" onClick={onSavePostClicked}>
+          Save Post
+        </button>
+      </>
+    )
+  }
+  return <section>{output}</section>
 }
